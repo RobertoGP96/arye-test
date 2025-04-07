571e7709aa3cb9ef47ec1a1db9841d186bf55b96
RobertoGP96 <trolbertogp96@gmail.com>
2025-04-07 10:30:39 -0400

    test theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Home from "./app/pages/Home";
 import Api from "./app/pages/Api";
 import Orders from "./app/pages/Orders";
 import Products from "./app/pages/Products";
-import { useState } from "react";
 import Shops from "./app/pages/Shops";
 import Users from "./app/pages/Users";
 import Packages from "./app/pages/Packages";
@@ -23,106 +22,77 @@ import Stores from "./app/pages/Stores";
 function App() {
   const pageNav = useNavigate();
 
-  const [active, setActive] = useState<number>(0);
 
   const items: MenuItem[] = [
     {
       label: "Home",
       icon: "pi pi-home",
-      className: active == 0 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/");
-        setActive(0);
       },
     },
     {
       label: "Pedidos",
       icon: "pi pi-shopping-cart",
-      className: active == 1 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/orders");
-        setActive(1);
       },
     },
     {
       label: "Productos",
       icon: "pi pi-warehouse",
-      className: active == 2 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/products");
-        setActive(2);
+
       },
     },
     {
       label: "Compras",
       icon: "pi pi-shopping-bag",
-      className: active == 3 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/shops");
-        setActive(3);
       },
     },
     {
       label: "Paquetes",
       icon: "pi pi-box",
-      className: active == 4 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/packages");
-        setActive(4);
       },
     },
     {
       label: "Entrega",
       icon: "pi pi-truck",
-      className: active == 5 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/delivery");
-        setActive(5);
       },
     },
     {
       label: "Estadísticas",
       icon: "pi pi-chart-bar",
-      className: active == 6 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/data");
-        setActive(6);
       },
     },
     {
       label: "Tiendas",
       icon: "pi pi-shop",
-      className: active == 7 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/stores");
-        setActive(7);
       },
     },
     {
       label: "Utiles",
       icon: "pi pi-wrench",
-      className: active == 8 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/tools");
-        setActive(8);
       },
     },
     {
       label: "Usuarios",
       icon: "pi pi-users",
-      className: active == 9 ? "bg-orange-400 rounded-sm" : "",
       command: () => {
         pageNav("/users");
-        setActive(9);
-      },
-    },
-    {
-      label: "Api",
-      icon: "pi pi-code",
-      className: (active == 10 ? "bg-orange-400 rounded-sm" : "") + " api-label-nav",
-      command: () => {
-        pageNav("/api");
-        setActive(10);
       },
     },
   ];
